6900e35b866973564f26b83d9b3c2688dfa5de94
Jermaine Janszen <jermainejanszen@gmail.com>
2019-06-30 19:31:24 +1000

    added loop to find name, price, and description of number of menu items

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 
 url = 'http://www.dawsonsespresso.com.au/'
+# driverPath = '/Users/jermaine/Desktop/chromedriver'
+driverPath = 'C:/Users/User/Desktop/chromedriver'
 
-driver = webdriver.Chrome('/Users/jermaine/Desktop/chromedriver')
+driver = webdriver.Chrome(driverPath)
 driver.get(url)
 
 menuButton = driver.find_element_by_id('glfButton0')
@@ -12,10 +14,18 @@ numSpecials = input('How many specials are there today?\n')
 numSpecials = int(numSpecials)
 print(numSpecials)
 
+menuItems = [[None for x in range(numSpecials)] for y in range(numSpecials)]
 
-menuItems = driver.find_elements_by_class_name('m-items')
 
-for i in range(0, numSpecials):
-    print(menuItems)
+for i in range(numSpecials):
+    itemName_element = driver.find_element_by_xpath('//*[@id="fb-content"]/app-restaurant/div/ui-view/app-menu/div/app-menu-items/div[1]/div[3]/div[{}]/div[1]/div[1]/div[2]'.format(i + 1))
+    menuItems[i][0] = itemName_element.text
+    itemDescription_element = driver.find_element_by_xpath('//*[@id="fb-content"]/app-restaurant/div/ui-view/app-menu/div/app-menu-items/div[1]/div[3]/div[{}]/div[1]/div[1]/div[3]'.format(i + 1))
+    menuItems[i][2] = itemDescription_element.text
+    itemPrice_element = driver.find_element_by_xpath('//*[@id="fb-content"]/app-restaurant/div/ui-view/app-menu/div/app-menu-items/div[1]/div[3]/div[{}]/div[1]/div[2]/div[1]/span'.format(i + 1))
+    menuItems[i][1] = itemPrice_element.text
+
+for i in range(numSpecials):
+    print(menuItems[i][0])
 
 driver.close()
