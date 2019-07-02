4fc695811b60981a4e4928d63474dcba98d89847
Jermaine Janszen <jermainejanszen@gmail.com>
2019-07-02 15:53:16 +1000

    completed run.py file

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 
 url = 'http://www.dawsonsespresso.com.au/'
-# driverPath = '/Users/jermaine/Desktop/chromedriver'
-driverPath = 'C:/Users/User/Desktop/chromedriver'
+# driverPath = '/Users/jermaine/Desktop/chromedriver'   # Mac
+driverPath = 'C:/Users/User/Desktop/chromedriver'       # Windows
 
 driver = webdriver.Chrome(driverPath)
 driver.get(url)
@@ -12,10 +12,10 @@ menuButton.click()
 
 numSpecials = input('How many specials are there today?\n')
 numSpecials = int(numSpecials)
-print(numSpecials)
+
+driver.switch_to.frame("gfOrderFrm")
 
 menuItems = [[None for x in range(numSpecials)] for y in range(numSpecials)]
-
 
 for i in range(numSpecials):
     itemName_element = driver.find_element_by_xpath('//*[@id="fb-content"]/app-restaurant/div/ui-view/app-menu/div/app-menu-items/div[1]/div[3]/div[{}]/div[1]/div[1]/div[2]'.format(i + 1))
@@ -24,8 +24,5 @@ for i in range(numSpecials):
     menuItems[i][2] = itemDescription_element.text
     itemPrice_element = driver.find_element_by_xpath('//*[@id="fb-content"]/app-restaurant/div/ui-view/app-menu/div/app-menu-items/div[1]/div[3]/div[{}]/div[1]/div[2]/div[1]/span'.format(i + 1))
     menuItems[i][1] = itemPrice_element.text
-
-for i in range(numSpecials):
-    print(menuItems[i][0])
 
 driver.close()
