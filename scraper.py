4eb171f352a41224e275cfdf35514588d822a933
jjan3640 <jjan3640@uni.sydney.edu.au>
2019-12-08 15:40:13 +1100

    moved scraping logic to its own file

---
+++ scraper.py
@@ -0,0 +1,56 @@
+import time
+import urllib
+import urllib.request
+# pip install selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+# Gets the lunch specials for the day
+# Returns a list of dictionaries each containing the
+#   name, description, and price of each menu item.
+def getMenuItems():
+    url = 'http://www.dawsonsespresso.com.au/'
+    driverPath = '/Users/jermaine/chromedriver'
+    chromeOptions = Options()
+    chromeOptions.headless = True
+
+    driver = webdriver.Chrome(driverPath, options=chromeOptions)
+    driver.get(url)
+
+    menuButton = driver.find_element_by_id('glfButton0')
+    menuButton.click()
+
+    time.sleep(5)
+
+    driver.switch_to.frame("gfOrderFrm")
+
+    menuItems = []
+
+    foundAllSpecials = False
+
+    items = driver.find_elements_by_xpath(
+        '//*[@id="fb-content"]/app-restaurant/div/ui-view/app-menu/div/app-menu-items/div[1]/div[3]')
+    itemsInfo = items[0].text.split('\n')
+
+    itemImages = []
+    for i in range(6):
+        try:
+            item = driver.find_element_by_xpath(
+                '//*[@id="fb-content"]/app-restaurant/div/ui-view/app-menu/div/app-menu-items/div[1]/div[3]/div[{}]/div/div[1]/div[1]'.format(i + 1))
+            itemImages.append(item.get_attribute('style')[23:-3])
+        except:
+            break
+
+    for i in range(int(len(itemsInfo)/3)):
+        menuItem = {
+            'name': itemsInfo[i*3], 'description': itemsInfo[i*3 + 1], 'price': itemsInfo[i*3 + 2]}
+        if 'Salad' not in menuItem['name']:
+            menuItems.append(menuItem)
+
+    for i in range(len(menuItems)):
+        urllib.request.urlretrieve(
+            itemImages[i], "images/{}.jpg".format(i + 1))
+
+    driver.quit()
+    return menuItems
--- view.py
+++ view.py
@@ -1,44 +1,32 @@
 import tkinter
-from run import getMenuItems
 # pip install pillow
 from PIL import Image, ImageTk
 
-# menuItems = getMenuItems()
 
-# for testing purposes
-menuItems = [{'name': 'Club Sandwich',
-              'description': 'chicken breast, bacon, tomato, lettuce, cheese, avocado & aioli on turkish with chips',
-              'price': '11.00'},
-             {'name': 'Fish & Chips',
-              'description': 'beer battered fish & chips with salad & tartare sauce',
-              'price': '11.00'},
-             {'name': 'Jalapeno Fish Tails & Chips',
-              'description': 'crumbed jalapeno fish tails served with chips, salad & tartare sauce',
-              'price': '11.00'}]
+# Creates the window to be displayed
+def createView(menuItems):
+    window = tkinter.Tk()
+    window.title("Lunch Specials")
+    screenWidth = window.winfo_screenwidth()
+    screenHeight = window.winfo_screenheight()
+    window.geometry('{}x{}'.format(screenWidth, screenHeight))
 
-window = tkinter.Tk()
-window.title("Lunch Specials")
-screenWidth = window.winfo_screenwidth()
-screenHeight = window.winfo_screenheight()
-window.geometry('{}x{}'.format(screenWidth, screenHeight))
+    itemsFrame = tkinter.Frame(window)
+    itemsFrame.pack(padx=50, pady=50)
+    for i in range(len(menuItems)):
+        item = menuItems[i]
+        rawImage = 'images/{}.jpg'.format(i+1)
+        itemPhoto = tkinter.PhotoImage(rawImage)
+        itemImage = tkinter.Label(itemsFrame, image=itemPhoto)
+        itemImage.grid(row=2*i, rowspan=2, column=0)
+        itemName = tkinter.Label(
+            itemsFrame, text=item['name'], fg='black')
+        itemName.grid(row=2*i, column=1, sticky='W')
+        itemDescription = tkinter.Label(
+            itemsFrame, text=item['description'], fg='gray')
+        itemDescription.grid(row=2*i+1, column=1, sticky='W')
+        itemPrice = tkinter.Label(
+            itemsFrame, text=item['price'], fg='black')
+        itemPrice.grid(row=2*i, column=2, sticky='E')
 
-itemsFrame = tkinter.Frame(window)
-itemsFrame.pack(padx=50, pady=50)
-for i in range(len(menuItems)):
-    item = menuItems[i]
-    rawImage = 'images/{}.jpg'.format(i+1)
-    itemPhoto = tkinter.PhotoImage(rawImage)
-    itemImage = tkinter.Label(itemsFrame, image=itemPhoto)
-    itemImage.grid(row=2*i, rowspan=2, column=0)
-    itemName = tkinter.Label(
-        itemsFrame, text=item['name'], fg='black')
-    itemName.grid(row=2*i, column=1, sticky='W')
-    itemDescription = tkinter.Label(
-        itemsFrame, text=item['description'], fg='gray')
-    itemDescription.grid(row=2*i+1, column=1, sticky='W')
-    itemPrice = tkinter.Label(
-        itemsFrame, text=item['price'], fg='black')
-    itemPrice.grid(row=2*i, column=2, sticky='E')
-
-
-window.mainloop()
+    window.mainloop()
