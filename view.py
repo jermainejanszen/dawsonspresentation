0ecc7a28ad5a694b2be0bb9be0a4519e66f36dc7
jjan3640 <jjan3640@uni.sydney.edu.au>
2019-12-08 17:26:53 +1100

    removed borders around images

--- view.py
+++ view.py
@@ -14,35 +14,27 @@ def createView(menuItems):
 
     dawsonLogoImage = Image.open('resources/DawsonLogo.gif')
     dawsonLogoPhoto = ImageTk.PhotoImage(dawsonLogoImage)
-    dawsonLogoLabel = tkinter.Label(window, image=dawsonLogoPhoto, bg='purple')
+    dawsonLogoLabel = tkinter.Label(window, image=dawsonLogoPhoto)
     dawsonLogoLabel.image = dawsonLogoPhoto
     dawsonLogoLabel.grid(row=0, column=0)
 
     itemsFrame = tkinter.Frame(window)
-    # itemsFrame.place(relx=0.1, rely=0.1, relheight=0.8, relwidth=0.8)
     itemsFrame.grid(row=1, column=0)
     for i in range(len(menuItems)):
         item = menuItems[i]
         rawImage = Image.open('images/{}.gif'.format(i+1))
         itemPhoto = ImageTk.PhotoImage(rawImage)
-        itemImage = tkinter.Label(itemsFrame, image=itemPhoto, bg='black')
+        itemImage = tkinter.Label(itemsFrame, image=itemPhoto)
         itemImage.image = itemPhoto
         itemImage.grid(row=2*i, rowspan=2, column=0)
-        # itemImage.place(relwidth=0.3, rely=i/len(menuItems), relx=0.0)
         itemName = tkinter.Label(
-            itemsFrame, text=item['name'], fg='black', bg='green')
+            itemsFrame, text=item['name'], fg='black')
         itemName.grid(row=2*i, column=1, sticky='W')
-        # itemName.place(relwidth=0.5, relheight=1 /
-        #               len(menuItems), rely=i/len(menuItems)/2, relx=0.3)
         itemDescription = tkinter.Label(
-            itemsFrame, text=item['description'], fg='gray', bg='red')
+            itemsFrame, text=item['description'], fg='gray')
         itemDescription.grid(row=2*i+1, column=1, sticky='W')
-        # itemDescription.place(relwidth=0.5, relheight=1/len(menuItems)/2,
-        #                      rely=i/len(menuItems) + 0.1, relx=0.3)
         itemPrice = tkinter.Label(
-            itemsFrame, text=item['price'], fg='black', bg='blue')
+            itemsFrame, text=item['price'], fg='black')
         itemPrice.grid(row=2*i, column=2, sticky='E')
-        # itemPrice.place(relwidth=0.2, relheight=1 /
-        #                len(menuItems), rely=i/len(menuItems), relx=0.8)
 
     window.mainloop()
